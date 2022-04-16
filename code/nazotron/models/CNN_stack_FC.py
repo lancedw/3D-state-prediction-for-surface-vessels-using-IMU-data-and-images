94707fc032848e84f91438498e38b0b778026b2c
lancedw <lancedewaele@icloud.com>
2022-04-16 12:19:15 +0200

    CNN training + testing + restructuring model_states folder

--- code/nazotron/models/CNN_stack_FC.py
+++ code/nazotron/models/CNN_stack_FC.py
@@ -3,18 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CNN_stack_FC(nn.Module):
-    def __init__(self, cuda = True, num_channel = 3,  cnn_fc_size = 1024, num_output=20, h_dim=2688, z_dim=1024):
+    def __init__(self, num_channel = 3,  cnn_fc_size = 1024, num_output=20, h_dim=2688, z_dim=1024):
         super(CNN_stack_FC, self).__init__()
-        self.cuda_p = cuda
         self.encoder = nn.Sequential(
             nn.Conv2d(num_channel, 8, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(8),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size= 3, stride=2, padding=1),
+
             nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2, padding = 1),
+
             nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(),
@@ -24,16 +25,15 @@ class CNN_stack_FC(nn.Module):
         self.mu = nn.Linear(h_dim, z_dim)
         self.std = nn.Linear(h_dim, z_dim)
 
-        self.dropout0 = nn.Dropout(p=0.3)
-        self.fc1 = nn.Linear(5376  , cnn_fc_size) #5376 / 20736
-
-        self.fc2 = nn.Linear(cnn_fc_size, 512) #5376 / 20736
+        self.fc1 = nn.Linear(5376  , cnn_fc_size) 
+        self.fc2 = nn.Linear(cnn_fc_size, 512) 
         self.fc3 = nn.Linear(512, 128)
         self.fc4 = nn.Linear(128, num_output)
-        self.dropout1 = nn.Dropout(p=0.3)
 
+        self.dropout0 = nn.Dropout(p=0.3)
+        self.dropout1 = nn.Dropout(p=0.3)
         self.dropout2 = nn.Dropout(p=0.4)
-        self.dropout3 = nn.Dropout(p= 0.4)
+        self.dropout3 = nn.Dropout(p=0.4)
 
 
     def reparameterize(self, mu, logvar):
