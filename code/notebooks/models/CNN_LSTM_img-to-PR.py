ff5108c24e78826d432e5714fb34991888ec2cd3
lancedw <lancedewaele@icloud.com>
2022-05-14 14:08:49 +0200

    Refactoring and moving models to seperate .py files

---
+++ code/notebooks/models/CNN_LSTM_img-to-PR.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN_encoder(nn.Module):
+    def __init__(self, channels=3):
+        super(CNN_encoder, self).__init__()
+
+        # outputs a tensor of shape (batch, out_channels, h/2/2, w/2/2)
+        self.cnn_encoder = nn.Sequential(
+            nn.Conv2d(in_channels=channels, out_channels=8, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+    
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+    def forward(self, x, batch_size):
+        # x is of shape (batch, seq_lenth, channels, height, width)
+        # iterate over the sequence and encode a feature vector for each image
+        img_features = [self.cnn_encoder(x[:,i,:,:,:].squeeze(1)) for i in range(x.size(1))]
+        
+        # flatten feature vectors in img_features
+        img_features = [img.reshape(batch_size, -1) for img in img_features]
+
+        # convert list of vectors into tensor of shape: (batch, len(img_features), features)
+        img_features = torch.stack(img_features, 1)
+
+        # reshape into (batch, 1, n*features)
+        img_features = img_features.reshape(batch_size, -1).unsqueeze(1)
+
+
+        return img_features
+
+
+# ENCODER
+class LSTM_encoder(nn.Module):
+    def __init__(self, input_size, hidden_size=1024, num_layers=1):
+        super(LSTM_encoder, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.input_size = input_size
+
+        self.encoder_lstm = nn.LSTM(
+            input_size=input_size, 
+            hidden_size=hidden_size, 
+            num_layers=num_layers,
+            batch_first=True,
+        )
+
+    def forward(self, input_seq):
+
+        ouputs, hidden = self.encoder_lstm(input_seq)
+        
+        return ouputs, hidden
+
+# DECODER
+class Decoder(nn.Module):
+    def __init__(self, output_size, hidden_size=1024, num_layers=1):
+        super(Decoder, self).__init__()
+
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.decoder_lstm = nn.LSTM(
+            input_size = hidden_size, 
+            hidden_size = hidden_size,
+            num_layers = num_layers, 
+            batch_first = True
+        )
+
+        self.decoder_fc_1 = nn.Linear(hidden_size, int(hidden_size/2))
+        self.decoder_fc_2 = nn.Linear(int(hidden_size/2), output_size)
+
+    def forward(self, x_input, hidden):
+        outputs = F.relu(x_input)
+
+        outputs, hidden = self.decoder_lstm(outputs, hidden)
+
+        outputs = F.relu(self.decoder_fc_1(outputs))
+        outputs = torch.tanh(self.decoder_fc_2(outputs))
+
+        return outputs, hidden
+
+
+# Wrapper class for encoder and decoder
+class CNN_LSTM_seq2seq(nn.Module):
+    def __init__(self, encoder_input_size, output_size, hidden_size = 1024):
+        super(CNN_LSTM_seq2seq, self).__init__()
+
+        self.input_size = encoder_input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+
+        self.cnn_encoder = CNN_encoder()
+        self.lstm_encoder = LSTM_encoder(input_size = encoder_input_size, hidden_size = hidden_size)
+        self.decoder = Decoder(output_size = output_size, hidden_size = hidden_size)
+
+    def forward(self, input_sequence):
+        batch_size = input_sequence.size(0)
+
+        features_vector = self.cnn_encoder(input_sequence, batch_size)
+
+        encoder_outputs, encoder_hidden  = self.lstm_encoder.forward(features_vector)
+
+        output, decoder_hidden = self.decoder.forward(encoder_outputs, encoder_hidden)
+
+        # reshape to (2, FRAMES_OUT) for two dimensional pitch and roll matrix
+        output = output.reshape(batch_size, 2, self.output_size/2)
+
+        return output
