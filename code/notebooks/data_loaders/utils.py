f427e1dc37e5e15128c9fe045e249bd5b1aa4f5f
Lance De Waele <Lance.DeWaele@UGent.be>
2022-05-26 13:22:45 +0200

    inference time

--- code/notebooks/data_loaders/utils.py
+++ code/notebooks/data_loaders/utils.py
@@ -1,3 +1,7 @@
+from re import M
+import torch
+import numpy as np
+
 class Utilities():
     
     # Function to normalize pixels
@@ -21,4 +25,60 @@ class Utilities():
     @staticmethod
     def denorm_pr(x, min = -90.0, max = 90.0):
         return ((x + 1) * (max - min))/2 + min
+
+
+    # function to measure inference time on GPU
+    @staticmethod
+    def inference_time(model, dummy_input, repetitions=10000):
+        device = torch.device("cuda")
+        model.to(device)
+        dummy_input.to(device)
+        # INIT LOGGERS
+        starter = torch.cuda.Event(enable_timing=True)
+        ender = torch.cuda.Event(enable_timing=True)
+        timings = np.zeros((repetitions, 1))
+        #GPU-WARM-UP
+        for _ in range(0):
+            _ = model(dummy_input)
+        # MEASURE PERFORMANCE
+        with torch.no_grad():
+            for rep in range(repetitions):
+                starter.record()
+                _ = model(dummy_input)
+                ender.record()
+                # WAIT FOR GPU SYNC
+                torch.cuda.synchronize()
+                curr_time = starter.elapsed_time(ender)
+                timings[rep] = curr_time
+
+        mean_syn = np.sum(timings) / repetitions
+        std_syn = np.std(timings)
+
+        return timings, mean_syn
+
+    # function to test if all (input, output) pairs are from an equal episode
+    @staticmethod
+    def test_sequence_integrity(all_sequences):
+        i = 0
+        for sequence in all_sequences:
+            input = sequence[0]
+            output = sequence[1]
+
+            episode = input.iloc[0]["episode"]
+            episode1 = output.iloc[0]["episode"]
+            
+            if(episode1 != episode):
+                print("Input and output from different episodes")
+                break
+
+            if (len(input['episode'].unique()) != 1):
+                print("input seq: "+i+" contains data from different episodes")
+                break
+            
+            if (len(output['episode'].unique()) != 1):
+                print("output seq: "+i+" contains data from different episodes")
+                break
+            
+            i += 1
+
     
---
+++ code/notebooks/models/CNN_LSTM_img_PR_to_PR.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN_encoder(nn.Module):
+
+    def __init__(self, channels=3):
+        super(CNN_encoder, self).__init__()
+
+        # outputs a tensor of shape (batch, out_channels, h/2/2, w/2/2)
+        self.cnn_encoder = nn.Sequential(
+            nn.Conv2d(in_channels=channels, out_channels=8, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+    
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+    def forward(self, img_sequence, pr_sequence, batch_size):
+        # x is of shape (batch, seq_lenth, channels, height, width)
+        # iterate over the sequence and encode a feature vector for each image
+        img_features = [self.cnn_encoder(img_sequence[:,i,:,:,:].squeeze(1)) for i in range(img_sequence.size(1))]
+        
+        # flatten individual feature vectors in img_features to (batch, n)
+        img_features = [img.reshape(batch_size, -1) for img in img_features]
+
+        # append pitch and roll to the end of each img feature vector
+        # pr_sequence is of shape (batch, seq_lenth, 2)
+        # TODO: change to other operation because cat is very slow on GPU
+        img_features = [torch.cat((img_features[i], pr_sequence[:,i,:]), dim=1) for i in range(len(img_features))]
+
+        # convert list of vectors into tensor of shape: (batch, len(img_features), features)
+        img_features = torch.stack(img_features, 1)
+
+        # reshape into (batch, 1, n*features)
+        img_features = img_features.reshape(batch_size, -1).unsqueeze(1)
+
+        return img_features
+
+
+# ENCODER
+class LSTM_encoder(nn.Module):
+    def __init__(self, input_size, hidden_size=1024, num_layers=1):
+        super(LSTM_encoder, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.input_size = input_size*4994
+
+        self.encoder_lstm = nn.LSTM(
+            input_size=input_size*4994, 
+            hidden_size=hidden_size, 
+            num_layers=num_layers,
+            batch_first=True,
+        )
+
+    def forward(self, input_seq):
+
+        ouputs, hidden = self.encoder_lstm(input_seq)
+        
+        return ouputs, hidden
+
+# DECODER
+class Decoder(nn.Module):
+    def __init__(self, output_size, hidden_size=1024, num_layers=1):
+        super(Decoder, self).__init__()
+
+        self.output_size = output_size*2
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.decoder_lstm = nn.LSTM(
+            input_size = hidden_size, 
+            hidden_size = hidden_size,
+            num_layers = num_layers, 
+            batch_first = True
+        )
+
+        self.decoder_fc_1 = nn.Linear(hidden_size, int(hidden_size/2))
+        self.decoder_fc_2 = nn.Linear(int(hidden_size/2), output_size*2)
+
+    def forward(self, x_input, hidden):
+
+        outputs, hidden = self.decoder_lstm(x_input, hidden)
+
+        outputs = self.decoder_fc_1(outputs)
+        outputs = self.decoder_fc_2(outputs)
+
+        return outputs, hidden
+
+
+# Wrapper class for encoder and decoder
+class CNN_LSTM_seq2seq(nn.Module):
+    def __init__(self, encoder_input_size, output_size, hidden_size = 1024):
+        super(CNN_LSTM_seq2seq, self).__init__()
+
+        self.input_size = encoder_input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+
+        self.cnn_encoder = CNN_encoder()
+        self.lstm_encoder = LSTM_encoder(input_size = encoder_input_size, hidden_size = hidden_size)
+        self.decoder = Decoder(output_size = output_size, hidden_size = hidden_size)
+
+    def forward(self, img_sequence, pr_sequence):
+        batch_size = pr_sequence.size(0)
+
+        features_vector = self.cnn_encoder(img_sequence, pr_sequence, batch_size)
+
+        encoder_outputs, encoder_hidden  = self.lstm_encoder.forward(features_vector)
+
+        output, decoder_hidden = self.decoder.forward(encoder_outputs, encoder_hidden)
+
+        output = output.reshape(batch_size, 2, self.output_size)
+
+        return output
